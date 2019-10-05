ef461deda8ce0308d97f40140baf3ec74a260b4b
Ramyaeaswaran <ramyaeaswaran77@gmail.com>
2019-10-05 06:05:15 +0530

    Movies_carouselslider

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,20 @@ import 'dart:convert';
 import 'movies.dart';
 import 'dart:core';
 import 'dart:math';
-
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(MaterialApp(
-    home:MoviesPage() ,
-
+    home: MoviesPage(),
   ),);
 }
 
 // You are given json string of movies list (see file movies.dart)
 String moviesList = MoviesList.moviesJsonList;
 List movies = jsonDecode(MoviesList.moviesJsonList);
+
 // Hint:
 // Convert the string to List of maps using jsonDecode and then use it
-
-
 
 // Create a stateful widget called MoviesPage here
 class MoviesPage extends StatefulWidget {
@@ -28,50 +26,61 @@ class MoviesPage extends StatefulWidget {
 }
 
 class _MoviesPageState extends State<MoviesPage> {
-  int a=1;
-  void changemovie(){
-    setState(() {
-     a= Random().nextInt(30);
-    }
-    );
-  }
-  @override
+  List<String> posters = [];
+
   Widget build(BuildContext context) {
-    List movie=jsonDecode(moviesList);
+   // int a = 0;
+  /*  void changemovie(){
+      setState(() {
+        a= Random().nextInt(30);
+      }
+      );*/
+    for (int i = 0; i < movies.length; i++) {
+      posters.add(movies[i]['poster']);
+    }
     return Scaffold(
-      backgroundColor: Colors.black ,
-      appBar: AppBar(
-        title: Text(
-          'Movies Poster',
-          style: TextStyle(color: Colors.black),
-        ),
         backgroundColor: Colors.yellow,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(left:50),
-        child: Column(
+        appBar: AppBar(
+          title: Text(
+            'Movies Poster',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.yellow,
+        ),
+        body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                child: Image(
-                  image: NetworkImage(movie[a]['poster']),
-                ),
-              ),
-              FlatButton(color: Colors.yellow,
-                child: new Text('Next Movie', textAlign: TextAlign.center),
-                onPressed: changemovie,
+              CarouselSlider(
+                height: 400,
+                aspectRatio: 16 / 9,
+//                autoPlay: true,
+//                autoPlayInterval: Duration(seconds: 3),
+//                autoPlayAnimationDuration: Duration(milliseconds: 800),
+ //               pauseAutoPlayOnTouch: Duration(seconds: 10),
+                //               enlargeCenterPage: true,
+                scrollDirection: Axis.horizontal,
+                items: posters.map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                          constraints: BoxConstraints.expand(),
+                          color: Colors.yellow,
+                          child: Image(
+                            image: NetworkImage(i),
+                          ));
+                    },
+                  );
+                }).toList(),
               ),
             ],
-          ),
-      ),
-
+        ),
     );
   }
 
-}
-void movie()
-{
-  List movies = jsonDecode(MoviesList.moviesJsonList);
-  for(var x in movies)
+/*  void movie() {
+    List movies = jsonDecode(MoviesList.moviesJsonList);
+    for (var x in movies)
     print(x['poster']);
+    }*/
 }
+
